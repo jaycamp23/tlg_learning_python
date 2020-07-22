7d3af507055910c04e49bfb0bda9735c99bf48c0
jaycamp23 <68555795+jaycamp23@users.noreply.github.com>
2020-07-22 20:05:18 +0000

    Add files via upload

---
+++ day_three_labs.py
@@ -0,0 +1,27 @@
+a = 2
+b = 3
+print(a + b)
+print( a * b)
+print(a - b)
+print(a / b)
+print(a % b)
+
+x = 'python'
+print(x[0])
+print(x[-1])
+print(len(x))
+x = x.upper()
+print(x)
+
+
+nums = []
+nums.append(10)
+nums.append(100)
+nums.append(10)
+nums.append(500)
+nums.append(56)
+nums.pop(-1)
+nums.sort()
+print(nums)
+
+
