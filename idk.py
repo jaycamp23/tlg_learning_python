003544aa490dbab5ef27afaa0d4f21b953cbcdd9
jaycamp23 <68555795+jaycamp23@users.noreply.github.com>
2020-07-23 13:50:12 +0000

    Add files via upload

---
+++ idk.py
@@ -0,0 +1,13 @@
+print('welcome to math class, 1 for yes 0 for no!')
+x = float(input('are you ready to learn:'))
+y = 1
+n = 0
+if x == n:
+    print('thats to bad')
+if x == y:
+    print('lets learn about hypotenuse')
+
+a = float(input("Input side a length: "))
+b = float(input("Input side b length: "))
+hypo = (a ** 2 + b ** 2) ** .5
+print("hypotenuse length is", hypo)
