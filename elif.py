89adb38ab387a978ce1abf746ebc3705f1a6972b
jaycamp23 <68555795+jaycamp23@users.noreply.github.com>
2020-07-23 15:50:09 +0000

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,10 @@
+age = 31
+age = float(input('how old are you:'))
+if age >= 35:
+    print('you are old enough to be a senator or the president')
+elif age >= 30:
+    print('you are old enough to be a senator or president')
+else:
+    print('your not old enough')
+
+print('have a nice day')
