90ed4647a8ea7902c17fefea80f9cb057d315730
jaycamp23 <68555795+jaycamp23@users.noreply.github.com>
2020-07-23 17:28:35 +0000

    Add files via upload

---
+++ lab16.py
@@ -0,0 +1,14 @@
+hostname = 'MTG'
+if hostname == 'MTG':
+    print('the hostname was found to be mtg')
+
+
+hostname = input('what value should we set for hostname?')
+if hostname == 'MTG':
+    print('the hostname was found to be mtg')
+if hostname.lower() == "mtg":
+    print('the hostname was found to be mtg')
+
+
+
+
